035cb59aa573d879e84e93078d7e54d46b1fa27c
Soud <78749228+Soud-Haroon@users.noreply.github.com>
2023-01-20 03:06:28 +0500

    qr_code

---
+++ lib/screens/home.dart
@@ -0,0 +1,43 @@
+// ignore_for_file: depend_on_referenced_packages
+
+import 'package:flutter/material.dart';
+import 'package:qr_flutter/qr_flutter.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      extendBodyBehindAppBar: false,
+      appBar: AppBar(title: const Text("Flutter QR Code")),
+      body: SafeArea(
+        child: Container(
+          margin: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+          width: double.infinity,
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(20),
+              color: Colors.grey.withOpacity(0.5)),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Center(
+                child: QrImage(
+                  data: 'https://www.instagram.com/soudharoon/',
+                  version: QrVersions.auto,
+                  size: 280,
+                  gapless: true,
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
